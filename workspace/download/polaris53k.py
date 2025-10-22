d8145b9d531dccb5c67423039c7660db6c99dfd1
rfechner <richard.fechner.nr@gmail.com>
2025-10-22 12:22:15 +0200

    added download scripts for dapo17k, polaris53k, fixed math500

--- workspace/download/polaris53k.py
+++ workspace/download/polaris53k.py
@@ -18,23 +18,15 @@ import datasets
 from verl.utils.hdfs_io import copy, makedirs
 import argparse
 
-from verl.utils.reward_score.math_reward import remove_boxed, last_boxed_only_string
-
-def extract_solution(solution_str):
-    return remove_boxed(last_boxed_only_string(solution_str))
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--local_dir', default='~/data/math500')
+    parser.add_argument('--local_dir', default='~/data/polaris53k')
     parser.add_argument('--hdfs_dir', default=None)
 
     args = parser.parse_args()
-    data_source = 'math-ai/math500'
+    data_source = "POLARIS-Project/Polaris-Dataset-53K"
     print(f"Loading the {data_source} dataset from huggingface...", flush=True)
-    dataset = datasets.load_dataset(data_source)
-
-    test_dataset = dataset['test']
+    dataset : datasets.DatasetDict = datasets.load_dataset(data_source)
     instruction_following = "Let's think step by step and output the final answer within \\boxed{}."
 
     # add a row to each data item that represents a unique id
@@ -45,13 +37,12 @@ if __name__ == '__main__':
 
             question = question + ' ' + instruction_following
 
-            solution = extract_solution(example.pop('solution'))
-            subject = example.pop('subject')
-            level = example.pop('level')
-            unique_id = example.pop('unique_id')
-            
+            answer = example.pop('answer')
+            difficulty = example.pop("difficulty")
+            solution = answer
+
             data = {
-                "data_source": data_source,
+                "data_source": 'polaris53k',
                 "prompt": [{
                     "role": "user",
                     "content": question
@@ -64,25 +55,20 @@ if __name__ == '__main__':
                 "extra_info": {
                     'split': split,
                     'index': idx,
-                    'subject': subject,
-                    'level': level,
-                    'unique_id': unique_id
+                    'difficulty' : difficulty
                 }
             }
             return data
 
         return process_fn
 
-    test_dataset = test_dataset.map(function=make_map_fn('test'), 
-                                    with_indices=True,
-                                    remove_columns=test_dataset.column_names)
+    train_dataset = dataset['train'].map(function=make_map_fn('train'), with_indices=True)
 
     local_dir = args.local_dir
     hdfs_dir = args.hdfs_dir
 
-    test_dataset.to_parquet(os.path.join(local_dir, 'test.parquet'))
+    train_dataset.to_parquet(os.path.join(local_dir, 'train.parquet'))
 
     if hdfs_dir is not None:
         makedirs(hdfs_dir)
-
         copy(src=local_dir, dst=hdfs_dir)
